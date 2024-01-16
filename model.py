b7fa2e0840a94638bbde086cd79c74634596fde6
im-strange <itsmestrange.dump@gmail.com>
2024-01-16 18:57:41 +0800

    update from pc

--- model.py
+++ model.py
@@ -13,15 +13,20 @@ data = list(csv.reader(open("emails.csv")))[1:]
 data_text = [" ".join(row[:-1]) for row in data]
 data_labels = [row[-1] for row in data]
 
+# split data into training and testing set
 X_train, X_test, Y_train, Y_test = train_test_split(data_text, data_labels, random_state=0)
 vectorizer = CountVectorizer()
 X_train = vectorizer.fit_transform(X_train)
 X_test = vectorizer.transform(X_test)
 
+# initialize a model
 model = MultinomialNB()
 model.fit(X_train, Y_train)
 
+# make a predictions with testing set
 predictions = model.predict(X_test)
+
+# get the prediction info
 accuracy = accuracy_score(Y_test, predictions)
 confusion_mat = confusion_matrix(Y_test, predictions)
 classification_rep = classification_report(Y_test, predictions)
